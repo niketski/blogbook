b24c575b6380b9c56668848203aca951f4f30872
niketski <lim.nicolebenedict3@gmail.com>
2024-11-04 20:56:05 +0800

    chore: initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        poppins: ['var(--font-poppins)']
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
