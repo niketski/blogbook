0bc5f761c18c07a7fca9c8fc412edf93c7aafa76
niketski <lim.nicolebenedict3@gmail.com>
2024-12-25 22:32:51 +0800

    feat: craeted simple ui for category page

--- src/app/(admin)/admin/blog/category/page.tsx
+++ src/app/(admin)/admin/blog/category/page.tsx
@@ -1,7 +1,138 @@
+import { Button } from "@/components/ui/button";
+import { Plus, Ellipsis } from "lucide-react";
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table";
+
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
 export default function BlogCategoryPage() {
     return (
         <div>
-            This is blog category page
+            <h1 className="font-bold text-4xl mb-10">Category</h1>
+            <Button><Plus/> Create Category</Button>
+            <div className="pt-[60px]">
+                <div className="w-full overflow-auto">
+                    <Table className="min-w-[800px]">
+                        <TableHeader>
+                            <TableRow>
+                                <TableHead>Name</TableHead>
+                                <TableHead>Slug</TableHead>
+                                <TableHead>Date</TableHead>
+                                <TableHead>
+                                    <span className="sr-only">Action Column</span>
+                                </TableHead>
+                            </TableRow>
+                        </TableHeader>
+                        <TableBody>
+                            <TableRow>
+                                <TableCell><span className="font-bold">Category 1</span></TableCell>
+                                <TableCell>category-1</TableCell>
+                                <TableCell>11/11/2024</TableCell>
+                                <TableCell>
+                                    <DropdownMenu>
+                                        <DropdownMenuTrigger asChild>
+                                            <Button variant="link"><Ellipsis/></Button>
+                                        </DropdownMenuTrigger>
+                                        <DropdownMenuContent>
+                                            <DropdownMenuLabel>Action</DropdownMenuLabel>
+                                            <DropdownMenuSeparator />
+                                            <DropdownMenuItem>Edit</DropdownMenuItem>
+                                            <DropdownMenuItem>Delete</DropdownMenuItem>
+                                        </DropdownMenuContent>
+                                    </DropdownMenu>
+                                </TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell><span className="font-bold">Category 2</span></TableCell>
+                                <TableCell>category-2</TableCell>
+                                <TableCell>11/11/2024</TableCell>
+                                <TableCell>
+                                    <DropdownMenu>
+                                        <DropdownMenuTrigger asChild>
+                                            <Button variant="link"><Ellipsis/></Button>
+                                        </DropdownMenuTrigger>
+                                        <DropdownMenuContent>
+                                            <DropdownMenuLabel>Action</DropdownMenuLabel>
+                                            <DropdownMenuSeparator />
+                                            <DropdownMenuItem>Edit</DropdownMenuItem>
+                                            <DropdownMenuItem>Delete</DropdownMenuItem>
+                                        </DropdownMenuContent>
+                                    </DropdownMenu>
+                                </TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell><span className="font-bold">Category 3</span></TableCell>
+                                <TableCell>category-3</TableCell>
+                                <TableCell>11/11/2024</TableCell>
+                                <TableCell>
+                                    <DropdownMenu>
+                                        <DropdownMenuTrigger asChild>
+                                            <Button variant="link"><Ellipsis/></Button>
+                                        </DropdownMenuTrigger>
+                                        <DropdownMenuContent>
+                                            <DropdownMenuLabel>Action</DropdownMenuLabel>
+                                            <DropdownMenuSeparator />
+                                            <DropdownMenuItem>Edit</DropdownMenuItem>
+                                            <DropdownMenuItem>Delete</DropdownMenuItem>
+                                        </DropdownMenuContent>
+                                    </DropdownMenu>
+                                </TableCell>
+                            </TableRow>
+                            {/* <TableRow>
+                                <TableCell><span className="font-bold">Sample Blog 2</span></TableCell>
+                                <TableCell>Category 2</TableCell>
+                                <TableCell>tag 2</TableCell>
+                                <TableCell>11/11/2024</TableCell>
+                                <TableCell>
+                                    <DropdownMenu>
+                                        <DropdownMenuTrigger asChild>
+                                            <Button variant="link"><Ellipsis/></Button>
+                                        </DropdownMenuTrigger>
+                                        <DropdownMenuContent>
+                                            <DropdownMenuLabel>Action</DropdownMenuLabel>
+                                            <DropdownMenuSeparator />
+                                            <DropdownMenuItem>Edit</DropdownMenuItem>
+                                            <DropdownMenuItem>Delete</DropdownMenuItem>
+                                        </DropdownMenuContent>
+                                    </DropdownMenu>
+                                </TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell><span className="font-bold">Sample Blog 3</span></TableCell>
+                                <TableCell>Category 3</TableCell>
+                                <TableCell>tag 3</TableCell>
+                                <TableCell>11/11/2024</TableCell>
+                                <TableCell>
+                                    <DropdownMenu>
+                                        <DropdownMenuTrigger asChild>
+                                            <Button variant="link"><Ellipsis/></Button>
+                                        </DropdownMenuTrigger>
+                                        <DropdownMenuContent>
+                                            <DropdownMenuLabel>Action</DropdownMenuLabel>
+                                            <DropdownMenuSeparator />
+                                            <DropdownMenuItem>Edit</DropdownMenuItem>
+                                            <DropdownMenuItem>Delete</DropdownMenuItem>
+                                        </DropdownMenuContent>
+                                    </DropdownMenu>
+                                </TableCell>
+                            </TableRow> */}
+                        </TableBody>
+                    </Table>
+                </div>
+            </div>
         </div>
     );
 }
