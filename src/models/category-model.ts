10c370e4e9bf984c6ef39d8c51b5496f32afef05
niketski <lim.nicolebenedict3@gmail.com>
2024-12-26 12:15:52 +0800

    feat: created category model

---
+++ src/models/category-model.ts
@@ -0,0 +1,21 @@
+import { model, Document, models, Schema } from 'mongoose';
+
+interface ICategory extends Document {
+    name: string,
+    slug: string,
+}
+
+const CategorySchema = new Schema<ICategory>({
+    name: {
+        type: String,
+        unique: true,
+    },
+    slug: {
+        type: String,
+        unique: true
+    }
+}, { timestamps: true });
+
+const CategoryModel = models.Category || model<typeof CategorySchema>('Category', CategorySchema);
+
+export default CategoryModel;
