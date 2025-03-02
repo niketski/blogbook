2bde36ef577cffdbd6acced7c6a9c560ad95b67e
niketski <lim.nicolebenedict3@gmail.com>
2025-03-02 17:40:53 +0800

    feat: includes deletion of image on cloudinary when a blog has deleted

--- src/actions/delete-blog.ts
+++ src/actions/delete-blog.ts
@@ -1,12 +1,22 @@
 'use server'
 
-import BlogModel from "@/models/blog-model";
+import cloudinaryHelper from "@/classes/cloudinary-helper";
+import BlogModel, { IBlog } from "@/models/blog-model";
 import { revalidatePath } from "next/cache";
 
 export default async function DeleteBlog(id: string){
     
     try {
 
+        const currentBlog = await BlogModel.findById<IBlog>(id);
+
+        // delete featured image from cloudinary
+        if(currentBlog && currentBlog.featuredImage) {
+            const featuredImageId = currentBlog.featuredImage.id;
+            
+            await cloudinaryHelper.remove(featuredImageId);
+        }
+        
         // delete blog
         await BlogModel.findByIdAndDelete(id);
 
