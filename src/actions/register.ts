adad066e33a10d0884f32df4e3d21168f7f53eae
niketski <lim.nicolebenedict3@gmail.com>
2025-04-19 16:48:56 +0800

    feat: added validation for each field

---
+++ src/actions/register.ts
@@ -0,0 +1,124 @@
+'use server'
+
+import z from 'zod';
+
+export interface registerState {
+    status: 'success' | 'error' | 'idle',
+    message: string,
+    values: {
+        name: string,
+        username: string,
+        email: string,
+        address: string,
+        password: string,
+        confirmPassword: string,
+    },
+    errors: {
+        name?: string[],
+        username?: string[],
+        email?: string[],
+        address?: string[],
+        password?: string[],
+        confirmPassword?: string[]
+        _form?: string
+    }
+}
+
+export default async function register(prevState: registerState, formData: FormData): Promise<registerState> {
+
+    const name = formData.get('name') as string;
+    const username = formData.get('username') as string;
+    const email = formData.get('email') as string;
+    const address = formData.get('address') as string;
+    const password = formData.get('password') as string; 
+    const confirmPassword = formData.get('confirmPassword') as string;
+        
+    try {
+
+        const formSchema = z.object({
+            name: z.string().trim().min(1, { message: 'Name is required' }).min(3, { message: 'Name must be atleast 3 characters'}),
+            username: z.string().min(1, { message: 'Name is required' }).min(3, { message: 'Username must be atleast 3 characters.' }),
+            email: z.string().min(1, { message: 'Email is required' }).email({message: 'Invalid email address.'}),
+            address: z.string().min(1, { message: 'Invalid address' }),
+            password: z.string().min(1, {message: 'Password is required'}).refine(val => /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/.test(val), {message: 'Please use a strong password'}),
+            confirmPassword: z.string().min(1, {message: 'Confirm password is required'}).refine(val => val === password, { message: 'The password don\'t match' }),
+        });
+        
+        const result = formSchema.safeParse({
+            name,
+            username,
+            email,
+            address,
+            password,
+            confirmPassword
+        });
+
+        if(!result.success) {
+            const currentErrors = result.error.flatten().fieldErrors;
+
+            return {
+                status: 'error',
+                values: {
+                    name,
+                    username,
+                    email,
+                    address,
+                    password,
+                    confirmPassword
+                },
+                message: 'Please make sure all of the fields are valid.',
+                errors: currentErrors
+            }
+        }
+
+        return {
+            status: 'success',
+            message: 'You have registered successfully!',
+            values: {
+                name: '',
+                username: '',
+                email: '',
+                address: '',
+                password: '',
+                confirmPassword: ''
+            },
+            errors: {}
+        }
+
+    } catch(error: unknown) {
+
+        if(error instanceof Error) {
+            return {
+                status: 'error',
+                message: 'Something went wrong.',
+                values: {
+                    name,
+                    username,
+                    email,
+                    address,
+                    password,
+                    confirmPassword
+                },
+                errors: {
+                    _form: error.message
+                }
+            }
+        }
+
+        return {
+            status: 'error',
+            message: 'Something went wrong.',
+            values: {
+                name,
+                username,
+                email,
+                address,
+                password,
+                confirmPassword
+            },
+            errors: {
+                _form: 'Error in submitting form.'
+            }
+        }
+    }
+}
--- src/app/(auth)/register/_components/registration-form.tsx
+++ src/app/(auth)/register/_components/registration-form.tsx
@@ -1,38 +1,112 @@
+'use client'
+
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
+import register from "@/actions/register";
+import { useActionState, useEffect } from "react";
+import { LoaderCircle } from "lucide-react";
 
 export default function RegistrationForm() {
+    const [formState, formAction, isPending] = useActionState(register, {
+        status: 'idle',
+        message: '',
+        values: {
+            name: '',
+            username: '',
+            email: '',
+            address: '',
+            password: '',
+            confirmPassword: '',
+        },
+        errors: {}
+    });
+    const status = formState.status;
+
+
+   useEffect(() => {
+
+        if(status === 'success') {
+            console.log('registered success fully.')
+        }
+
+   }, [status]);
+
     return (
-        <form>
+        <form action={formAction}>
             <div className="grid w-full items-center gap-1.5 mb-4">
                 <Label htmlFor="name" className="font-bold">Name</Label>
-                <Input type="text" id="name" placeholder="Name" />
-                {/* <p className="text-xs text-red-500">error message.</p> */}
+                <Input 
+                    className={`${formState.errors.name ? 'border-red-500' : ''}`}
+                    type="text" 
+                    id="name" 
+                    name="name" 
+                    placeholder="Name" 
+                    defaultValue={formState.values.name}/>
+                {formState.errors.name && <p className="text-xs text-red-500">{formState.errors.name.join(', ')}</p>}
             </div>
-            <div className="flex gap-4">
+            <div className="flex gap-4 items-start">
                 <div className="grid w-1/2 items-center gap-1.5 mb-4">
                     <Label htmlFor="username" className="font-bold">Username</Label>
-                    <Input type="text" id="username" placeholder="Username" />
-                    {/* <p className="text-xs text-red-500">error message.</p> */}
+                    <Input 
+                        className={`${formState.errors.username ? 'border-red-500' : ''}`}
+                        type="text" 
+                        id="username" 
+                        name="username" 
+                        placeholder="Username" 
+                        defaultValue={formState.values.username}/>
+                    {formState.errors.username && <p className="text-xs text-red-500">{formState.errors.username.join(', ')}</p>}
                 </div>
                 <div className="grid w-1/2 items-center gap-1.5 mb-4">
                     <Label htmlFor="email" className="font-bold">Email</Label>
-                    <Input type="email" id="email" placeholder="Email" />
-                    {/* <p className="text-xs text-red-500">error message.</p> */}
+                    <Input 
+                        className={`${formState.errors.email ? 'border-red-500' : ''}`}
+                        type="email" 
+                        id="email" 
+                        name="email" 
+                        placeholder="Email" 
+                        defaultValue={formState.values.email}/>
+                    {formState.errors.email && <p className="text-xs text-red-500">{formState.errors.email.join(', ')}</p>}
                 </div>
             </div>
             <div className="grid w-full items-center gap-1.5 mb-4">
                 <Label htmlFor="address" className="font-bold">Address</Label>
-                <Input type="text" id="address" placeholder="Addresss" />
-                {/* <p className="text-xs text-red-500">error message.</p> */}
+                <Input 
+                    className={`${formState.errors.address ? 'border-red-500' : ''}`}
+                    type="text" 
+                    id="address" 
+                    name="address" 
+                    placeholder="Street / district or barangay / city" 
+                    defaultValue={formState.values.address}/>
+                {formState.errors.address && <p className="text-xs text-red-500">{formState.errors.address.join(', ')}</p>}
             </div>
             <div className="grid w-full items-center gap-1.5 mb-4">
                 <Label htmlFor="password" className="font-bold">Password</Label>
-                <Input type="password" id="password" placeholder="Password" />
-                {/* <p className="text-xs text-red-500">error message.</p> */}
+                <Input 
+                    className={`${formState.errors.password ? 'border-red-500' : ''}`}
+                    type="password" 
+                    id="password" 
+                    name="password" 
+                    placeholder="Password" 
+                    defaultValue={formState.values.password}/>
+                {formState.errors.password && <p className="text-xs text-red-500">{formState.errors.password.join(', ')}</p>}
             </div>
-            <Button type="submit">Submit</Button>
+            <div className="grid w-full items-center gap-1.5 mb-4">
+                <Label htmlFor="confirmPassword" className="font-bold">Confirm password</Label>
+                <Input 
+                    className={`${formState.errors.name ? 'border-red-500' : ''}`}
+                    type="password" 
+                    id="confirmPassword" 
+                    name="confirmPassword" 
+                    placeholder="Confirm password" 
+                    defaultValue={formState.values.confirmPassword}/>
+                {formState.errors.confirmPassword && <p className="text-xs text-red-500">{formState.errors.confirmPassword.join(', ')}</p>}
+            </div>
+                <Button type="submit">
+                {isPending ? 
+                     <>Processing <LoaderCircle className="animate-spin"/></>: 
+                    'Submit'}
+                </Button>
         </form>
     );
 }
