ce261d468d71d221a860a37a763afb641eeaf821
Nicole Benedict Lim <limnicolebenedict3@gmail.com>
2025-05-01 18:03:37 +0800

    feat: created middleware authenticate admin access and added callback url

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,1 +1,18 @@
-export { auth as middleware } from '@/lib/auth';
+import { NextResponse } from "next/server";
+import { auth } from "./lib/auth";
+import { NextRequest } from "next/server";
+
+export async function middleware(req: NextRequest) {
+    const session = await auth();
+    const callbackUrl = req.nextUrl.pathname;
+    
+   if(session) {
+        return NextResponse.next();
+    }
+
+    return NextResponse.redirect(new URL(`/login?callbackUrl=${callbackUrl}`, req.url));
+}
+
+export const config = {
+    matcher: ['/admin/:path*'], // Just dynamic routes, not static files
+  };
