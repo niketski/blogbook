88b5fcd80d6e408bb459bb4c56d9b69e6e5fc5ce
Nicole Benedict Lim <limnicolebenedict3@gmail.com>
2025-05-04 10:15:51 +0800

    feat: created featured blog component

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,6 +1,18 @@
+import FeaturedBlog from "@/components/featured-blog";
 
 export default function Home() {
   return (
-    <h1>This is homepage</h1>
+    <div className="py-[80px]">
+      <div className="mx-auto max-w-[1170px] px-5">
+        <h1 className="text-center font-bold text-4xl mb-10">Latest Blogs</h1>
+        <FeaturedBlog
+          title="Lorem ipsum"
+          date="9/3/2025"
+          category="Category"
+          imageUrl="https://res.cloudinary.com/dndtvwfvg/image/upload/v1738101686/blogbook/iisuwbpvzcsuya56lxru.jpg"
+          link="#"
+          excerpt="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dol..."/>
+      </div>
+    </div>
   );
 }
