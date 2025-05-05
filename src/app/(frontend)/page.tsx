d713e20a65f8d63bd0cdf69a73e6ed97a5908a4d
Nicole Benedict Lim <limnicolebenedict3@gmail.com>
2025-05-05 16:48:19 +0800

    feat: created hover effect on blog cards images

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,17 +1,40 @@
 import FeaturedBlog from "@/components/featured-blog";
+import BlogCard from "@/components/blog-card";
 
 export default function Home() {
   return (
     <div className="py-[80px]">
       <div className="mx-auto max-w-[1170px] px-5">
         <h1 className="text-center font-bold text-4xl mb-10">Latest Blogs</h1>
-        <FeaturedBlog
-          title="Lorem ipsum"
-          date="9/3/2025"
-          category="Category"
-          imageUrl="https://res.cloudinary.com/dndtvwfvg/image/upload/v1738101686/blogbook/iisuwbpvzcsuya56lxru.jpg"
-          link="#"
-          excerpt="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dol..."/>
+        <div className="p-2">
+          <FeaturedBlog
+            title="Lorem ipsum"
+            date="9/3/2025"
+            category="Category"
+            imageUrl="https://res.cloudinary.com/dndtvwfvg/image/upload/v1738101686/blogbook/iisuwbpvzcsuya56lxru.jpg"
+            link="#"
+            excerpt="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dol..."/>
+        </div>
+        <div className="md:flex md:flex-wrap">
+          <div className="md:w-1/2 p-2">
+            <BlogCard
+              title="Lorem ipsum"
+              date="9/3/2025"
+              category="Category"
+              imageUrl="https://res.cloudinary.com/dndtvwfvg/image/upload/v1738101686/blogbook/iisuwbpvzcsuya56lxru.jpg"
+              link="#"
+              excerpt="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dol..."/>
+          </div>
+          <div className="md:w-1/2 p-2">
+            <BlogCard
+              title="Lorem ipsum"
+              date="9/3/2025"
+              category="Category"
+              imageUrl="https://res.cloudinary.com/dndtvwfvg/image/upload/v1738101686/blogbook/iisuwbpvzcsuya56lxru.jpg"
+              link="#"
+              excerpt="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dol..."/>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/components/blog-card.tsx
+++ src/components/blog-card.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 
-interface FeaturedBlogProps {
+interface BlogCardProps {
     title: string;
     date: string;
     category?: string;
@@ -11,24 +11,24 @@ interface FeaturedBlogProps {
     link: string;
 }
 
-export default function FeaturedBlog({ 
+export default function BlogCard({ 
     title,
     date,
     category,
     excerpt,
     imageUrl,
     link
- }: FeaturedBlogProps) {
+ }: BlogCardProps) {
     
     return (
-        <div className="flex border border-gray-300 rounded-lg overflow-hidden">
-          <div className="w-1/2">
+        <div className="border-gray-300 rounded-lg overflow-hidden">
+          <div className="w-full">
             <Link href="#" className="block  relative">
-              <canvas width="564" height="400" className="w-full"></canvas>
+              <canvas width="549" height="300" className="w-full"></canvas>
               <img src={imageUrl} alt={title} className="absolute top-0 left-0 w-full h-full object-cover object-center grayscale transition duration-300 ease-in-out hover:grayscale-0"/>
             </Link>
           </div>
-          <div className="w-1/2 p-10">
+          <div className="w-full p-10">
             <Badge className="mb-3">{category}</Badge>
             <h2 className="font-bold text-2xl mb-2">{title}</h2>
             <span className="text-gray-500 text-sm mb-5 block">{date}</span>
