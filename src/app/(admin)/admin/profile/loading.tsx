d455f9076d7cf4a989197e4cea9ad6de172db7a0
Nicole Benedict Lim <limnicolebenedict3@gmail.com>
2025-05-19 20:41:49 +0800

    feat: created loaders for admin profile and edit blog page

---
+++ src/app/(admin)/admin/profile/loading.tsx
@@ -0,0 +1,11 @@
+import { Skeleton } from "@/components/ui/skeleton";
+import SkeletonProfileCard from "@/components/skeleton/skeleton-profile-card";
+
+export default function ProfilePageLoading() {
+    return (
+        <div>
+            <h1 className="font-bold text-4xl mb-10">User</h1>
+            <SkeletonProfileCard/>
+        </div>
+    );
+}
--- src/app/(admin)/admin/profile/page.tsx
+++ src/app/(admin)/admin/profile/page.tsx
@@ -1,49 +1,12 @@
-import { auth } from "@/lib/auth";
-import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardHeader,
-    CardTitle
-} from '@/components/ui/card';
-import UserModel from "@/models/user-model";
+import ProfileInfoCard from "./_components/profile-info-card";
 
 export default async function ProfilePage() {
-    const session = await auth();
-    const userId = session && session.user ? session.user.id : null;
-    const user = await UserModel.findById({ _id: userId });
+   
 
     return (
         <div>
             <h1 className="font-bold text-4xl mb-10">User</h1>
-            <Card>
-                <CardHeader>
-                    <CardTitle>User info.</CardTitle>
-                    <CardDescription>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </CardDescription>
-                </CardHeader>
-                <CardContent>
-                    <div className="md:flex mb-3">
-                        <div>
-                            <strong>Name:</strong>
-                            <p>{user?.name}</p>
-                        </div>
-                    </div>
-                    <div className="md:flex">
-                        <div className="md:w-1/2 mb-3">
-                            <strong>Username:</strong>
-                            <p>{user?.username}</p>
-                        </div>
-                        <div className="md:w-1/2 mb-3">
-                            <strong>Email:</strong>
-                            <p>{user?.email}</p>
-                        </div>
-                    </div>
-                    <div>
-                        <strong>Address:</strong>
-                        <p>{user?.address}</p>
-                    </div>
-                </CardContent>
-            </Card>
+            <ProfileInfoCard/>
         </div>
     );
 }
